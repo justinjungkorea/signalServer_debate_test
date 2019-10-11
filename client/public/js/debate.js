e62608024dec4af376509ebeb0e1e6b2c15e023f
dongwook jung <jdw8998@daum.net>
2019-10-11 18:00:59 +0900

    1011 done!

--- client/public/js/debate.js
+++ client/public/js/debate.js
@@ -6,23 +6,36 @@ function getUrlParams() {
   return params;
 }
 
-var room;
-
 var data = {
+  room: '',
   nickname: '',
-  message: ' 입장 하였습니다.'
+  message: ' 입장'
 };
 
-window.onload = ()=>{
-  var params = getUrlParams();
-  data.message = params.person;
-  room = params.room;
+var params = getUrlParams();
+if(params.person==='1'){
+  data.nickname = '사회자';
+} else if(params.person === '2'){
+  data.nickname = '참여자A';
+} else {
+  data.nickname = '참여자B';
 }
+data.room = params.room;
 
+
+socket.emit('info', data);
+
+socket.emit('chat message', data);
 
 socket.on('chat message', (data) => {
   $('#messages').append($('<li>').text(data.nickname+": "+data.message));
   window.scrollTo(0, document.body.scrollHeight);
 });
 
-socket.emit('chat message', data);
+$('form').submit((e)=>{
+  e.preventDefault();
+  data.message = $('#m').val();
+  socket.emit('chat message', data);
+  $('#m').val('');
+  return false;
+});
